8611ff6a2ed8fd43a6757bab7ee01469235530d5
Andreas Seficha <andreas.seficha@marketplace-analytics.de>
2016-11-17 15:33:41 +0100

    Test server app

---
+++ src/main/java/com/sellics/mavenproject1/NewServlet.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sellics.mavenproject1;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.sellics.monsum.Customer;
+import com.sellics.monsum.MonsumHttpClient;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author dre
+ */
+public class NewServlet extends HttpServlet {
+    Customer cus;
+    MonsumHttpClient client;
+  //  private static Logger myLogger = Logger.getLogger("web"); 
+
+
+    @Override 
+    public void init() throws ServletException {
+        client=new MonsumHttpClient();
+        cus=new Customer(client.getClient());
+      //  myLogger.setLevel(Level.ALL);
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+          String ret=  cus.getByNumber(Integer.parseInt((request.getParameter("customer_number"))));
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+JsonParser jp = new JsonParser();
+JsonElement je = jp.parse(ret);
+String prettyJsonString = gson.toJson(je);
+            /* TODO output your page here. You may use following sample code. */
+              String title = "Using GET Method to Read Number and Output Json";
+      String docType =
+      "<!doctype html public \"-//w3c//dtd html 4.0 " +
+      "transitional//en\">\n";
+      out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>Customer</b>: "
+                    + "<pre>"+prettyJsonString+ "</pre>"+"\n"+
+               
+                "</ul>\n" +
+                "</body></html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    @Override
+    public void destroy(){
+     client.shutdown();
+    }
+}
